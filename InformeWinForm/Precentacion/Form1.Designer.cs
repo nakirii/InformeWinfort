8dc02a5fa14dda31a9ece75d9b7a95d7b5d2ee0e
Hernando_O <hernandoorozco03@gmail.com>
2022-08-10 12:18:56 -0500

    update 3

--- InformeWinForm/Precentacion/Form1.Designer.cs
+++ InformeWinForm/Precentacion/Form1.Designer.cs
@@ -30,9 +30,9 @@ namespace Precentacion
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource5 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource6 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource7 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource8 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource9 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.SalesReportBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.SaleListingBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.NetSaleByPeriodBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -70,15 +70,15 @@ namespace Precentacion
             this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            reportDataSource4.Name = "salesReport";
-            reportDataSource4.Value = this.SalesReportBindingSource;
-            reportDataSource5.Name = "salesListing";
-            reportDataSource5.Value = this.SaleListingBindingSource;
-            reportDataSource6.Name = "netSalesByPeriod";
-            reportDataSource6.Value = this.NetSaleByPeriodBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource4);
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource5);
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource6);
+            reportDataSource7.Name = "salesReport";
+            reportDataSource7.Value = this.SalesReportBindingSource;
+            reportDataSource8.Name = "salesListing";
+            reportDataSource8.Value = this.SaleListingBindingSource;
+            reportDataSource9.Name = "netSalesByPeriod";
+            reportDataSource9.Value = this.NetSaleByPeriodBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource7);
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource8);
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource9);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "Precentacion.Report.ReportSales.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(220, 12);
             this.reportViewer1.Name = "reportViewer1";
@@ -184,7 +184,7 @@ namespace Precentacion
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(168, 31);
             this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
+            this.button2.Text = "biton actuqlizado";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
