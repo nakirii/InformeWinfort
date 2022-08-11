a4ece2bf14d1b65ef13378d8b37409a4898c957a
Hernando_O <hernandoorozco03@gmail.com>
2022-08-10 23:03:57 -0500

    ultimate update

--- InformeWinForm/Precentacion/Form1.Designer.cs
+++ InformeWinForm/Precentacion/Form1.Designer.cs
@@ -46,6 +46,7 @@ namespace Precentacion
             this.btnMes = new System.Windows.Forms.Button();
             this.btnult7 = new System.Windows.Forms.Button();
             this.btnhoy = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.SalesReportBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SaleListingBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NetSaleByPeriodBindingSource)).BeginInit();
@@ -90,6 +91,7 @@ namespace Precentacion
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.dateTimefin);
             this.panel1.Controls.Add(this.dateTimeinicio);
@@ -185,6 +187,16 @@ namespace Precentacion
             this.btnhoy.UseVisualStyleBackColor = true;
             this.btnhoy.Click += new System.EventHandler(this.btnhoy_Click_1);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(15, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 31);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "generar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -218,6 +230,7 @@ namespace Precentacion
         private System.Windows.Forms.Button btnMes;
         private System.Windows.Forms.Button btnult7;
         private System.Windows.Forms.Button btnhoy;
+        private System.Windows.Forms.Button button2;
     }
 }
 
